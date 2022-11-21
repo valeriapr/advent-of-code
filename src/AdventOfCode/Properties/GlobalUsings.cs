e62b97bfc8203208fe55e00b3d32b5d921fc6e0b
Valeria Prakopchyk <v.prakopchyk@tjip.com>
2022-11-21 15:39:38 +0100

    Setup of solution.

---
+++ src/AdventOfCode/Properties/GlobalUsings.cs
@@ -0,0 +1,7 @@
+global using NUnit.Framework;
+global using FluentAssertions;
+global using System;
+global using System.Collections.Generic;
+global using System.Linq;
+global using System.Text;
+global using System.Threading.Tasks;
